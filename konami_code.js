36b3094d540e5cfec0fbe4e4717e330c2397612a
Kyle Samuel <ksamuel92@gmail.com>
2021-02-14 18:58:46 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,14 +12,14 @@ const codes = [
 ];
 
 function init() {
-  let index = 0;
+  let index = 0; //establish index
   
-  document.body.addEventListener("keydown", function(e) {
-    const key = e.key;
+  document.body.addEventListener("keydown", function(e) { //add eventlistener for keydown event
+    const key = e.key; //assign event.key to the key variable
     
-    if (key === codes[index]) {
+    if (key === codes[index]) { // if key (read: key pressed) is equal to the 0 index, add to index
       index++;
-      if (index === codes.length) 
+      if (index === codes.length) //if index reaches the end of the codes array, alert user
       {
         alert("Hurray!");
         
